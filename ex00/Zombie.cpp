c08b592c85da69dd94e2aeb98cb7b10ba2f7f252
lydia <lydiavdvegt@gmail.com>
2025-07-09 16:42:09 +0200

    created zombie class and constructor

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 15:59:02 by lyvan-de          #+#    #+#             */
+/*   Updated: 2025/07/09 16:07:22 by lyvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) {
+	std::cout << name << ": BraiiiiiiinnnzzzZ...";
+}
+
