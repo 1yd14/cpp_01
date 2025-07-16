9b0a6b094f8013a2367197b1c3e93d8015c6a4a7
lydia <lydiavdvegt@gmail.com>
2025-07-16 16:35:48 +0200

    made ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 16:18:58 by lyvan-de          #+#    #+#             */
+/*   Updated: 2025/07/16 16:34:52 by lyvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+int	main(void) {
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+	std::cout << std::left;
+	std::cout << std::setw(35) << "address of string: " << &string << std::endl;
+	std::cout << std::setw(35) << "address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << std::setw(35) << "address of stringREF: " << &stringREF << std::endl;
+	std::cout << std::setw(35) << "value of string: " << string << std::endl;
+	std::cout << std::setw(35) << "value pointed to by stringPTD: " << *stringPTR << std::endl;
+	std::cout << std::setw(35) << "value pointed to by stringREF:  " << stringREF << std::endl;
+}
