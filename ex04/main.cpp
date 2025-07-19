a742e264121be5979ca6953ddb40f64da746dd8c
Lydia van der Vegt <lydiavdvegt@gmail.com>
2025-07-19 17:04:51 +0200

    finished the exercise and some test files

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 17:51:03 by lyvan-de          #+#    #+#             */
-/*   Updated: 2025/07/19 16:32:51 by lyvan-de         ###   ########.fr       */
+/*   Updated: 2025/07/19 17:00:12 by lyvan-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 #include <iostream>
 #include <fstream>
 
-//bool	fileExists(const std::string& filename) {
-//	std::ifstream file(filename);
-//	return file.good();
-//}
-
 std::string	replaceString(const std::string line, const std::string target, const std::string replace) {
 	std::string	result;
 	size_t		pos;
 	size_t		findPos;
-	
+
 	pos = 0;
+	if (target.empty())
+		return line;
 	while((findPos = line.find(target, pos)) != std::string::npos) {
 		result.append(line, pos, findPos - pos);
 		result.append(replace);
@@ -38,7 +35,7 @@ int	main(int argc, char* argv[]){
 	std::string	filename;
 	std::string	s1;
 	std::string	s2;
-	std::string	output;
+	std::string	line;
 
 	if (argc != 4) {
 		std::cout << "Input incorrect" << std::endl;
@@ -46,17 +43,22 @@ int	main(int argc, char* argv[]){
 	filename = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
-	//std::ifstream inFile(filename);
-	//if (!inFile) {
-	//	std::cerr << "cannot read from file" << std::endl;
-	//	return 1;
-	//}
-	//std::ofstream outFile(filename + ".replace");
-	//if (!outFile) {
-	//	std::cerr << "Cannot write to file" << std::endl;
-	//	return 1;
-	//}
-	output = replaceString(filename, s1, s2);
-	std::cout << output << std::endl;
+	std::ifstream inFile(filename);
+	if (!inFile) {
+		std::cerr << "cannot read from file" << std::endl;
+		return 1;
+	}
+	std::ofstream outFile(filename + ".replace");
+	if (!outFile) {
+		std::cerr << "Cannot write to output file" << std::endl;
+		return 1;
+	}
+	while (std::getline(inFile, line)) {
+		line = replaceString(line, s1, s2);
+		outFile << line << '\n';
+	}
+	inFile.close();
+	outFile.close();
+	return 0;
 };
 
